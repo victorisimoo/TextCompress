a4c92470033b8cd0403e285a91dc68cb0d7e4926
victorisimoo <vmelendrez2013241@gmail.com>
2020-10-20 19:24:17 -0600

    Decompress module

--- LZWCompress/Structure/FileController.cs
+++ LZWCompress/Structure/FileController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 
 namespace LZWCompress.Controllers {
+
     public class FileController : ICompressor {
 
 
@@ -109,7 +110,71 @@ namespace LZWCompress.Controllers {
 
         public string Decompress(IFormFile file, string routeDirectory) {
 
-            return " ";
+            var DiccionarioLetras = new Dictionary<int, string>();
+            var bufferLenght = 10000;
+            var byteBuffer = new byte[bufferLenght];
+            var bufferEscritura = new List<byte>();
+            var auxActual = string.Empty;
+            var auxPrevio = string.Empty;
+            var aux = string.Empty;
+            var first = true;
+
+            
+            if (!Directory.Exists(Path.Combine(routeDirectory, "decompress"))) {
+                Directory.CreateDirectory(Path.Combine(routeDirectory, "decompress"));
+            }
+
+            using (var reader = new BinaryReader(file.OpenReadStream())) {
+                using (var streamWriter = new FileStream(Path.Combine(routeDirectory, "decompress", $"{Path.GetFileNameWithoutExtension(file.FileName)}.txt"), FileMode.OpenOrCreate)) {
+                    using (var writer = new BinaryWriter(streamWriter)) {
+
+                        byteBuffer = reader.ReadBytes(8);
+                        var CantDiccionario = Convert.ToInt32(Encoding.UTF8.GetString(byteBuffer));
+                        for (int i = 0; i < CantDiccionario; i++) {
+                            byteBuffer = reader.ReadBytes(1);
+                            var letra = Convert.ToChar(byteBuffer[0]).ToString();
+                            DiccionarioLetras.Add(DiccionarioLetras.Count() + 1, letra);
+                        }
+
+                        byteBuffer = reader.ReadBytes(1);
+                        var cantidadBits = Convert.ToInt32(byteBuffer[0]);
+
+                        while (reader.BaseStream.Position != reader.BaseStream.Length) {
+                            byteBuffer = reader.ReadBytes(bufferLenght);
+                            foreach (var item in byteBuffer) {
+                                aux += Convert.ToString(item, 2).PadLeft(8, '0'); ;
+                                while (aux.Length >= cantidadBits){
+                                    var nuevoNum = Convert.ToInt32(aux.Substring(0, cantidadBits), 2);
+                                    if (nuevoNum != 0) {
+                                        if (first){
+                                            first = false;
+                                            auxPrevio = DiccionarioLetras[nuevoNum];
+                                            bufferEscritura.Add(Convert.ToByte(Convert.ToChar(auxPrevio)));
+                                        }else {
+                                            if (nuevoNum > DiccionarioLetras.Count) {
+                                                auxActual = auxPrevio + auxPrevio.First();
+                                                DiccionarioLetras.Add(DiccionarioLetras.Count + 1, auxActual);
+                                            }else {
+                                                auxActual = DiccionarioLetras[nuevoNum];
+                                                DiccionarioLetras.Add(DiccionarioLetras.Count + 1, $"{auxPrevio}{auxActual.First()}");
+                                            }
+
+                                            foreach (var letra in auxActual) {
+                                                bufferEscritura.Add(Convert.ToByte(letra));
+                                            }
+
+                                            auxPrevio = auxActual;
+                                        }
+                                    }
+                                    aux = aux.Substring(cantidadBits);
+                                }
+                            }
+                        }
+                        writer.Write(bufferEscritura.ToArray());
+                    }
+                }
+            }
+                return Path.Combine(routeDirectory, "decompress", Path.GetFileNameWithoutExtension(file.FileName));
         }
     }
 }
